04284dafbb4c996d98f29a94aad58dfe6d5fcc3a
Simon Kennedy <sffjunkie+code@gmail.com>
2025-11-14 16:21:23 +0000

    Initial commit. Created from siteconfig repo

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          go = import ./go { inherit pkgs; };
+          python = import ./python { inherit pkgs; };
+          rust = import ./rust { inherit pkgs; };
+          net = import ./net { inherit pkgs; };
+        }
+      );
+    };
+}
